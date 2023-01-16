2224663e83a9efd95446c6d8f4f1eb45311c3644
michivonah <info@michivonah>
2023-01-16 21:15:10 +0100

    enable landing page #3

--- game.js
+++ game.js
@@ -18,7 +18,8 @@ let scores = [ // The scoreboard
 ];
 
 function startGame(){
-    //window.scroll(0, window.innerHeight);
+    window.scroll(0, window.innerHeight);
+    document.getElementById('home').style.display = "none";
     shuffleCards();
     distributeCards(player0, 0);
     distributeCards(player1, 1);
