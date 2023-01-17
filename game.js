7dedf468828d67270be7466c47030b78c677b6b2
michivonah <info@michivonah>
2023-01-17 20:59:06 +0100

    update calculations and include trumps #2

--- game.js
+++ game.js
@@ -36,217 +36,253 @@ function shuffleCards(){
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/eichel-ass.gif",
             "Type": "A",
             "Color": "Eichel",
-            "Points": "11"
+            "Points": "11",
+            "PointsTrump": "11"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/eichel-koenig.gif",
             "Type": "K",
             "Color": "Eichel",
-            "Points": "4"
+            "Points": "4",
+            "PointsTrump": "4"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/eichel-ober.gif",
             "Type": "O",
             "Color": "Eichel",
-            "Points": "3"
+            "Points": "3",
+            "PointsTrump": "3"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/eichel-under.gif",
             "Type": "U",
             "Color": "Eichel",
-            "Points": "20"
+            "Points": "2",
+            "PointsTrump": "20"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/eichel-banner.gif",
             "Type": "B",
             "Color": "Eichel",
-            "Points": "10"
+            "Points": "10",
+            "PointsTrump": "10"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/eichel-neun.gif",
             "Type": "9",
             "Color": "Eichel",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "14"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/eichel-acht.gif",
             "Type": "8",
             "Color": "Eichel",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "0"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/eichel-sieben.gif",
             "Type": "7",
             "Color": "Eichel",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "0"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/eichel-sechs.gif",
             "Type": "6",
             "Color": "Eichel",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "0"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/rosen-ass.gif",
             "Type": "A",
             "Color": "Rose",
-            "Points": "11"
+            "Points": "11",
+            "PointsTrump": "11"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/rosen-koenig.gif",
             "Type": "K",
             "Color": "Rose",
-            "Points": "4"
+            "Points": "4",
+            "PointsTrump": "4"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/rosen-ober.gif",
             "Type": "O",
             "Color": "Rose",
-            "Points": "3"
+            "Points": "3",
+            "PointsTrump": "3"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/rosen-under.gif",
             "Type": "U",
             "Color": "Rose",
-            "Points": "20"
+            "Points": "2",
+            "PointsTrump": "20"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/rosen-banner.gif",
             "Type": "B",
             "Color": "Rose",
-            "Points": "10"
+            "Points": "10",
+            "PointsTrump": "10"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/rosen-neun.gif",
             "Type": "9",
             "Color": "Rose",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "14"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/rosen-acht.gif",
             "Type": "8",
             "Color": "Rose",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "0"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/rosen-sieben.gif",
             "Type": "7",
             "Color": "Rose",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "0"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/rosen-sechs.gif",
             "Type": "6",
             "Color": "Rose",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "0"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schellen-ass.gif",
             "Type": "A",
             "Color": "Schelle",
-            "Points": "11"
+            "Points": "11",
+            "PointsTrump": "11"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schellen-koenig.gif",
             "Type": "K",
             "Color": "Schelle",
-            "Points": "4"
+            "Points": "4",
+            "PointsTrump": "4"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schellen-ober.gif",
             "Type": "O",
             "Color": "Schelle",
-            "Points": "3"
+            "Points": "3",
+            "PointsTrump": "3"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schellen-under.gif",
             "Type": "U",
             "Color": "Schelle",
-            "Points": "20"
+            "Points": "2",
+            "PointsTrump": "20"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schellen-banner.gif",
             "Type": "B",
             "Color": "Schelle",
-            "Points": "10"
+            "Points": "10",
+            "PointsTrump": "10"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schellen-neun.gif",
             "Type": "9",
             "Color": "Schelle",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "14"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schellen-acht.gif",
             "Type": "8",
             "Color": "Schelle",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "0"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schellen-sieben.gif",
             "Type": "7",
             "Color": "Schelle",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "0"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schellen-sechs.gif",
             "Type": "6",
             "Color": "Schelle",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "0"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schilten-ass.gif",
             "Type": "A",
             "Color": "Schilte",
-            "Points": "11"
+            "Points": "11",
+            "PointsTrump": "11"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schilten-koenig.gif",
             "Type": "K",
             "Color": "Schilte",
-            "Points": "4"
+            "Points": "4",
+            "PointsTrump": "4"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schilten-ober.gif",
             "Type": "O",
             "Color": "Schilte",
-            "Points": "3"
+            "Points": "3",
+            "PointsTrump": "3"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schilten-under.gif",
             "Type": "U",
             "Color": "Schilte",
-            "Points": "20"
+            "Points": "2",
+            "PointsTrump": "20"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schilten-banner.gif",
             "Type": "B",
             "Color": "Schilte",
-            "Points": "10"
+            "Points": "10",
+            "PointsTrump": "10"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schilten-neun.gif",
             "Type": "9",
             "Color": "Schilte",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "14"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schilten-acht.gif",
             "Type": "8",
             "Color": "Schilte",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "0"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schilten-sieben.gif",
             "Type": "7",
             "Color": "Schilte",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "0"
         },
         {
             "imageUrl": "https://jass.von-ah.me/assets/cards/jassverzeichnis/schilten-sechs.gif",
             "Type": "6",
             "Color": "Schilte",
-            "Points": "0"
+            "Points": "0",
+            "PointsTrump": "0"
         },
     ];
     
@@ -289,6 +325,7 @@ function distributeCards(cards, playerNumber){
     newCard.src = cards[i].imageUrl;
     newCard.classList = "card";
     newCard.dataset.points = cards[i].Points;
+    newCard.dataset.pointsTrump = cards[i].PointsTrump;
     newCard.dataset.type = cards[i].Type;
     newCard.dataset.color = cards[i].Color;
     newCard.dataset.playedFrom = playerNumber;
@@ -300,11 +337,26 @@ function distributeCards(cards, playerNumber){
 }
 
 function generateTrump(){
-    trump = Math.floor(Math.random() * 3);
+    var random = Math.floor(Math.random() * 3);
+    var trumps = [
+        {
+            "trumpColor": "Eichel"
+        },
+        {
+            "trumpColor": "Rose"
+        },
+        {
+            "trumpColor": "Schelle"
+        },
+        {
+            "trumpColor": "Schilte"
+        }
+    ];
+    trump = trumps[random].trumpColor;
     var trumpIcon = document.getElementById("currentTrump");
-    if(trump == 0) trumpIcon.src = "assets/cards/eichel.svg";
-    else if(trump == 0) trumpIcon.src = "assets/cards/rose.svg";
-    else if(trump == 0) trumpIcon.src = "assets/cards/schelle.svg";
+    if(trump == "Eichel") trumpIcon.src = "assets/cards/eichel.svg";
+    else if(trump == "Rose") trumpIcon.src = "assets/cards/rose.svg";
+    else if(trump == "Schelle") trumpIcon.src = "assets/cards/schelle.svg";
     else trumpIcon.src = "assets/cards/schilte.svg";
 }
 
@@ -357,7 +409,13 @@ function countPoints(){
     var winnerTeam;
     for(var card = 1; card < cards.length; card++){
         var playedCard = cards[card];
-        var cardPoints = parseInt(playedCard.dataset.points);
+        if(trump == playedCard.dataset.color){
+            var cardPoints = parseInt(playedCard.dataset.pointsTrump);
+            console.log("Trump given");
+        }
+        else{
+            var cardPoints = parseInt(playedCard.dataset.points);
+        }
         if(cardPoints > highestValue){
             highestValue = cardPoints;
             winnerTeam = parseInt(playedCard.dataset.playedFrom);
